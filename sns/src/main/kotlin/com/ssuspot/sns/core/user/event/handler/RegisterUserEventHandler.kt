fd64f26b9a1c85a11993191741731e5e24ac034a
julymeltdown <quintuplets2000@gmail.com>
2023-09-13 11:11:23 +0900

    [BE FIX] Event 발행 코드 수정

--- sns/src/main/kotlin/com/ssuspot/sns/core/user/event/handler/RegisterUserEventHandler.kt
+++ sns/src/main/kotlin/com/ssuspot/sns/core/user/event/handler/RegisterUserEventHandler.kt
@@ -1,6 +1,7 @@
 package com.ssuspot.sns.core.user.event.handler
 
 import com.ssuspot.sns.core.user.event.RegisteredUserEvent
+import org.springframework.context.event.EventListener
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 import org.springframework.transaction.event.TransactionPhase
@@ -9,15 +10,15 @@ import org.springframework.transaction.event.TransactionalEventListener
 @Component
 class RegisterUserEventHandler {
     //생성된 유저에 대해 타임라인 생성 요청
+    @EventListener
     @Async
-    @TransactionalEventListener(phase= TransactionPhase.AFTER_COMMIT)
     fun createTimeline(event: RegisteredUserEvent) {
         println("create timeline for User")
     }
 
     //성공 메일 발송
+    @EventListener
     @Async
-    @TransactionalEventListener(phase= TransactionPhase.AFTER_COMMIT)
     fun sendSuccessMail(event: RegisteredUserEvent) {
         println("sendSuccessMail")
     }
