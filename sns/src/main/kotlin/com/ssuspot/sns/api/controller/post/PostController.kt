891d0b16ad38cab1b189d40cdfb7be9ed0ae08fd
julymeltdown <quintuplets2000@gmail.com>
2023-09-21 03:09:47 +0900

    [BE FEAT] get my post api 구현

--- sns/src/main/kotlin/com/ssuspot/sns/api/controller/post/PostController.kt
+++ sns/src/main/kotlin/com/ssuspot/sns/api/controller/post/PostController.kt
@@ -3,18 +3,43 @@ package com.ssuspot.sns.api.controller.post
 import com.ssuspot.sns.application.dto.post.CreatePostDto
 import com.ssuspot.sns.api.request.post.CreatePostRequest
 import com.ssuspot.sns.api.response.post.PostResponse
+import com.ssuspot.sns.application.dto.post.GetPostsDto
 import com.ssuspot.sns.application.service.post.PostService
 import com.ssuspot.sns.infrastructure.security.UserPrincipal
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class PostController(
     private val postService: PostService
 ) {
+    @GetMapping("/api/posts")
+    fun getMyPosts(
+        @RequestParam("page", defaultValue = "1") page: Int,
+        @RequestParam("size", defaultValue = "10") size: Int,
+        @AuthenticationPrincipal userDetails: UserPrincipal
+    ): ResponseEntity<List<PostResponse>> {
+        val posts = postService.getPostsByUserId(GetPostsDto(page, size, userDetails.username))
+        return ResponseEntity.ok(
+            posts.content.map {
+                PostResponse(
+                    it.id!!,
+                    it.title,
+                    it.content,
+                    it.user.email,
+                    it.imageUrls,
+                    it.spot.id!!
+                )
+            }
+        )
+    }
+
+
     @PostMapping("/api/post")
     fun createPost(
         @RequestBody request: CreatePostRequest,
--- sns/src/main/kotlin/com/ssuspot/sns/application/service/post/PostService.kt
+++ sns/src/main/kotlin/com/ssuspot/sns/application/service/post/PostService.kt
@@ -2,10 +2,13 @@ package com.ssuspot.sns.application.service.post
 
 import com.ssuspot.sns.application.dto.post.CreatePostDto
 import com.ssuspot.sns.application.dto.post.CreatePostResponseDto
+import com.ssuspot.sns.application.dto.post.GetPostsDto
 import com.ssuspot.sns.application.service.spot.SpotService
 import com.ssuspot.sns.application.service.user.UserService
 import com.ssuspot.sns.domain.model.post.entity.Post
 import com.ssuspot.sns.infrastructure.repository.post.PostRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Service
 
 @Service
@@ -37,5 +40,11 @@ class PostService(
             imageUrls = savedPost.imageUrls,
             spotId = savedPost.spot.id!!
         )
+    }
+
+    fun getPostsByUserId(getPostsRequest: GetPostsDto): Page<Post> {
+        val user = userService.findValidUserByEmail(getPostsRequest.email)
+        val pageable = PageRequest.of(getPostsRequest.page - 1, getPostsRequest.size)
+        return postRepository.findPostsByUserUserId(user.id!!, pageable)
     }
 }
