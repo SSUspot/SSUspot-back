243cada5901f57fb53ff4f3375b629ce6e7e5d4b
julymeltdown <quintuplets2000@gmail.com>
2023-10-01 17:19:23 +0900

    [BE FEAT] get comment by post id controller 구현 + 페이징 적용

--- sns/src/main/kotlin/com/ssuspot/sns/api/controller/post/CommentController.kt
+++ sns/src/main/kotlin/com/ssuspot/sns/api/controller/post/CommentController.kt
@@ -3,19 +3,42 @@ package com.ssuspot.sns.api.controller.post
 import com.ssuspot.sns.api.request.post.CreateCommentRequest
 import com.ssuspot.sns.api.response.post.CommentResponse
 import com.ssuspot.sns.application.dto.post.CreateCommentDto
+import com.ssuspot.sns.application.dto.post.GetCommentDto
 import com.ssuspot.sns.application.service.post.CommentService
 import com.ssuspot.sns.infrastructure.security.UserPrincipal
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class CommentController(
     private val commentService: CommentService,
 ) {
+    @GetMapping("/api/posts/{postId}/comments")
+    fun getCommentsByPostId(
+        @PathVariable postId: Long,
+        @RequestParam("page", defaultValue = "1") page: Int,
+        @RequestParam("size", defaultValue = "10") size: Int,
+    ): ResponseEntity<List<CommentResponse>> {
+        val comments = commentService.getCommentsByPostId(
+            GetCommentDto(
+                postId = postId,
+                page = page,
+                size = size
+            )
+        )
+        return ResponseEntity.ok(
+            comments.map {
+                CommentResponse(
+                    id = it.id,
+                    postId = it.postId,
+                    userEmail = it.userEmail,
+                    content = it.content,
+                )
+            }
+        )
+    }
+
     @PostMapping("/api/posts/{postId}/comments")
     fun createComment(
         @PathVariable postId: Long,
