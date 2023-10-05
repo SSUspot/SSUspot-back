d0e7a29301bd48e583df8865452bb7a78cad4ef5
julymeltdown <quintuplets2000@gmail.com>
2023-10-05 17:41:56 +0900

    commit error fixed

--- sns/build/tmp/kapt3/stubs/main/com/ssuspot/sns/domain/exceptions/user/UserPasswordIncorrectException.java
+++
@@ -1,9 +0,0 @@
-package com.ssuspot.sns.domain.exceptions.user;
-
-@kotlin.Metadata(mv = {1, 8, 0}, k = 1, xi = 48, d1 = {"\u0000\u0010\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\u0018\u00002\u00060\u0001j\u0002`\u0002B\u0005\u00a2\u0006\u0002\u0010\u0003\u00a8\u0006\u0004"}, d2 = {"Lcom/ssuspot/sns/domain/exceptions/user/UserPasswordIncorrectException;", "Ljava/lang/RuntimeException;", "Lkotlin/RuntimeException;", "()V", "sns"})
-public final class UserPasswordIncorrectException extends java.lang.RuntimeException {
-    
-    public UserPasswordIncorrectException() {
-        super();
-    }
-}
