ccf36cc21fc37d7bbbfad2ed0b5e47bacf516ead
julymeltdown <quintuplets2000@gmail.com>
2023-11-18 17:00:42 +0900

    [BE CHORE] 캐시 사용하도록 설정

---
+++ sns/src/main/kotlin/com/ssuspot/sns/infrastructure/configs/RedisCacheConfig.kt
@@ -0,0 +1,32 @@
+package com.ssuspot.sns.infrastructure.configs
+
+import org.springframework.cache.CacheManager
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.cache.RedisCacheConfiguration
+import org.springframework.data.redis.cache.RedisCacheManager
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.RedisSerializationContext
+import org.springframework.data.redis.serializer.StringRedisSerializer
+import java.time.Duration
+
+
+@Configuration
+@EnableCaching
+class RedisCacheConfig {
+    @Bean
+    fun contentCacheManager(cf: RedisConnectionFactory?): CacheManager {
+        val redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+            .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer<String>(StringRedisSerializer()))
+            .serializeValuesWith(
+                RedisSerializationContext.SerializationPair.fromSerializer<Any>(
+                    GenericJackson2JsonRedisSerializer()
+                )
+            ) // Value Serializer 변경
+            .entryTtl(Duration.ofMinutes(3L)) // 캐시 수명 30분
+        return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(cf!!)
+            .cacheDefaults(redisCacheConfiguration).build()
+    }
+}
---
+++ sns/src/main/kotlin/com/ssuspot/sns/infrastructure/configs/RedisTemplateConfig.kt
@@ -0,0 +1,28 @@
+package com.ssuspot.sns.infrastructure.configs
+
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.client.BufferingClientHttpRequestFactory
+import org.springframework.http.client.SimpleClientHttpRequestFactory
+import org.springframework.http.converter.StringHttpMessageConverter
+import org.springframework.web.client.RestTemplate
+import java.nio.charset.StandardCharsets
+
+
+@Configuration
+class RedisTemplateConfig {
+    @Bean
+    fun restTemplate(restTemplateBuilder: RestTemplateBuilder): RestTemplate {
+        return restTemplateBuilder
+            .requestFactory {
+                BufferingClientHttpRequestFactory(
+                    SimpleClientHttpRequestFactory()
+                )
+            }.additionalMessageConverters(
+                StringHttpMessageConverter(
+                    StandardCharsets.UTF_8
+                )
+            ).build()
+    }
+}
