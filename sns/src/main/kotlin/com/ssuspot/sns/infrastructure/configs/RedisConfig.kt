30e79c9f8a091a3b155b1305a383fc96d42b04bc
julymeltdown <quintuplets2000@gmail.com>
2023-11-26 15:44:21 +0900

    [BE CHORE] Redis 비밀번호 설정

--- sns/src/main/kotlin/com/ssuspot/sns/infrastructure/configs/RedisConfig.kt
+++ sns/src/main/kotlin/com/ssuspot/sns/infrastructure/configs/RedisConfig.kt
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
 
 @Configuration
 @EnableRedisRepositories
@@ -17,14 +20,23 @@ class RedisConfig {
     @Value("\${spring.redis.port}")
     private val redisPort = 0
 
+    @Value("\${spring.redis.password}")
+    private val password: String? = null
+
     @Bean
-    fun redisConnectionFactory(): RedisConnectionFactory{
-        return LettuceConnectionFactory(redisHost!!, redisPort)
+    fun redisConnectionFactory(): RedisConnectionFactory {
+        val redisConfiguration = RedisStandaloneConfiguration()
+        redisConfiguration.hostName = redisHost!!
+        redisConfiguration.port = redisPort
+        redisConfiguration.setPassword(password)
+        return LettuceConnectionFactory(redisConfiguration)
     }
 
     @Bean
-    fun redisTemplate(): RedisTemplate<String, String>{
+    fun redisTemplate(): RedisTemplate<String, String> {
         val redisTemplate = RedisTemplate<String, String>()
+        redisTemplate.keySerializer = StringRedisSerializer()
+        redisTemplate.valueSerializer = StringRedisSerializer()
         redisTemplate.setConnectionFactory(redisConnectionFactory())
         return redisTemplate
     }
