4b6133e1c1cb4230458000582152cffae31ac305
julymeltdown <quintuplets2000@gmail.com>
2023-12-07 18:37:03 +0900

    [BE REFACTOR] 이미지 서비스 코드 리팩토링 (함수 분리)

--- sns/src/main/kotlin/com/ssuspot/sns/application/service/post/ImageService.kt
+++ sns/src/main/kotlin/com/ssuspot/sns/application/service/post/ImageService.kt
@@ -25,76 +25,43 @@ class ImageService(
 ) {
     fun uploadSingleImage(image: MultipartFile, email: String): ImageDto {
         val user = userService.findValidUserByEmail(email)
-        val savedImageLink = uploadSingleFile(image)
-        val metadata = ImageMetadataReader.readMetadata(image.inputStream)
-        val directory = metadata.getFirstDirectoryOfType(com.drew.metadata.exif.GpsDirectory::class.java)
-        val latitude = directory?.getGeoLocation()?.latitude ?: 0.0
-        val longitude = directory?.getGeoLocation()?.longitude?: 0.0
-
+        val (latitude, longitude) = extractGeoLocation(image)
+        val savedImageLink = uploadFileToS3(image)
 
         val savedImage = imageRepository.save(
-            Image(
-                imageUrl = savedImageLink.imageUrls,
-                latitude = latitude,
-                longitude = longitude,
-                user = user
-            )
+            Image(imageUrl = savedImageLink, latitude = latitude, longitude = longitude, user = user)
         )
-        return ImageDto(
-            savedImage.id!!,
-            savedImage.user.id!!,
-            savedImage.imageUrl
-        )
+        return ImageDto(savedImage.id!!, savedImage.user.id!!, savedImage.imageUrl)
     }
+
     fun uploadMultipleFiles(files: List<MultipartFile>, email: String): List<ImageDto> {
-        val imageUrls = mutableListOf<String>()
         val user = userService.findValidUserByEmail(email)
+        val (latitude, longitude) = extractGeoLocation(files.first())
 
-        val metadata = ImageMetadataReader.readMetadata(files[0].inputStream)
-        val directory = metadata.getFirstDirectoryOfType(com.drew.metadata.exif.GpsDirectory::class.java)
-        val latitude = directory?.getGeoLocation()?.latitude ?: 0.0
-        val longitude = directory?.getGeoLocation()?.longitude?: 0.0
+        val savedImages = files.map { uploadFileToS3(it) }.map { imageUrl ->
+            Image(imageUrl = imageUrl, user = user, latitude = latitude, longitude = longitude)
+        }.let { imageRepository.saveAll(it) }
 
-
-        for (file in files) {
-            val fileObj: File = convertMultiPartFileToFile(file)
-            val fileName = System.currentTimeMillis().toString() + "_" + file.originalFilename
-            s3Client.putObject(PutObjectRequest(bucketName, fileName, fileObj))
-            imageUrls.add(s3Client.getUrl(bucketName, fileName).toString())
-            fileObj.delete()
-        }
-        val savedImages = imageRepository.saveAll(
-            imageUrls.map {
-                Image(
-                    imageUrl = it,
-                    user = user,
-                    latitude = latitude,
-                    longitude = longitude
-                )
-            }
-        )
-
-        return savedImages.map {
-            ImageDto(
-                it.id!!,
-                it.user.id!!,
-                it.imageUrl
-            )
-        }
+        return savedImages.map { ImageDto(it.id!!, it.user.id!!, it.imageUrl) }
     }
-    //추후 이미지 여러개 업로드로 수정 가능성 있음
-    fun uploadSingleFile(file: MultipartFile): StorageResponseDto {
-        val imageUrls = mutableListOf<String>()
-        val fileObj: File = convertMultiPartFileToFile(file)
+
+    private fun uploadFileToS3(file: MultipartFile): String {
+        val fileObj = convertMultiPartFileToFile(file)
         val fileName = System.currentTimeMillis().toString() + "_" + file.originalFilename
         s3Client.putObject(PutObjectRequest(bucketName, fileName, fileObj))
-        imageUrls.add(s3Client.getUrl(bucketName, fileName).toString())
+        val imageUrl = s3Client.getUrl(bucketName, fileName).toString()
         fileObj.delete()
-        return StorageResponseDto(imageUrls.joinToString(","))
+        return imageUrl
+    }
+
+    private fun extractGeoLocation(file: MultipartFile): Pair<Double, Double> {
+        val metadata = ImageMetadataReader.readMetadata(file.inputStream)
+        val directory = metadata.getFirstDirectoryOfType(com.drew.metadata.exif.GpsDirectory::class.java)
+        return Pair(directory?.getGeoLocation()?.latitude ?: 0.0, directory?.getGeoLocation()?.longitude ?: 0.0)
     }
 
     private fun convertMultiPartFileToFile(file: MultipartFile): File {
-        val convertedFile = File(file.originalFilename) ?: throw FileNotFoundException()
+        val convertedFile = File(file.originalFilename ?: throw FileNotFoundException("Original filename not found"))
         FileOutputStream(convertedFile).use { fos -> fos.write(file.bytes) }
         return convertedFile
     }
